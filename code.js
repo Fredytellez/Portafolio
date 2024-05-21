571dae2522a6b5642864ff54e5340aa064d21aa0
Fredy Pena <tredy@outlook.com>
2024-05-21 09:35:33 -0500

    Primer commit Portafolio, problemas del css en la page Sobre Mi

---
+++ code.js
@@ -0,0 +1,11 @@
+// Inicializa el lottle de la imagen de perfil animada
+console.log("La pagina esta conectada a Javascript");
+
+lottie.loadAnimation({
+  container: document.getElementById("lottie-animation"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "GD6214u0Qc.json",
+});
+
